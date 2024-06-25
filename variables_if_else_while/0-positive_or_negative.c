d494b19ffc61b241b57b08b22851a6fdf9673a06
Fareedizm <guliyevfarid013.com>
2024-06-25 09:08:10 +0000

    c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+int n;
+
+
+if(n>=0){
+    printf("%d is positive\n", n);
+}
+if(n=0){
+    printf("%d is zero\n" , n);
+    
+}
+if(n<0){
+    printf("%d is negative" ,n);
+}
+
+
+
+return (0);
+}
