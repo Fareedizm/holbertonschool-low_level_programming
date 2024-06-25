07530c000ded770088cead3e9e27e682c3990640
Fareedizm <guliyevfarid013.com>
2024-06-25 10:11:57 +0000

    c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
+/**
+  * main - Prints a hexadecimal string
+  *
+  * Return: Always (Success)
+  */
+
+
 int main(void)
 {
-    char c;
+	char c;
 
-    for (c = '0'; c <= '9'; c++)  // Print digits '0' to '9'
-        putchar(c);
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
 
-    for (c = 'a'; c <= 'f'; c++)  // Print lowercase letters 'a' to 'f'
-        putchar(c);
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
 
-    putchar('\n');  // Print a newline character at the end
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
-
