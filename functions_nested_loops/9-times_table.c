4002dd7177287b128898bbef267c58f70ecc5231
Fareedizm <guliyevfarid013.com>
2024-07-10 18:57:40 +0000

    C

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -21,9 +21,13 @@ void times_table(void)
                 _putchar(',');
                 _putchar(' ');
                 if (product < 10)
+                {
                     _putchar(' ');
+                }
                 else
+                {
                     _putchar((product / 10) + '0');
+                }
                 _putchar((product % 10) + '0');
             }
         }
