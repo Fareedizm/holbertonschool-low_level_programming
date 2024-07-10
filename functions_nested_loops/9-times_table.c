cbfb0f53c1f376f3e1af84dc78005dc841ac51a1
Fareedizm <guliyevfarid013.com>
2024-07-10 18:56:04 +0000

    C

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,19 +12,18 @@ void times_table(void)
         for (j = 0; j < 10; j++)
         {
             product = i * j;
-            if (j != 0)
-                _putchar(',');
-            if (product < 10)
+            if (j == 0)
             {
-                if (j != 0)
-                    _putchar(' ');
-                _putchar(' ');
                 _putchar(product + '0');
             }
             else
             {
+                _putchar(',');
                 _putchar(' ');
-                _putchar((product / 10) + '0');
+                if (product < 10)
+                    _putchar(' ');
+                else
+                    _putchar((product / 10) + '0');
                 _putchar((product % 10) + '0');
             }
         }
