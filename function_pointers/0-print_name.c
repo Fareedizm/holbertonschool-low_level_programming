65365e01497610907bfc229a31638b0648146669
Fareedizm <guliyevfarid013.com>
2024-07-17 13:32:16 +0400

    function_pointers

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -7,28 +7,3 @@ void print_name(char *name, void (*f)(char *))
         f(name);
 }
 
-void print_name_as_is(char *name)
-{
-    printf("Hello, my name is %s\n", name);
-}
-
-void print_name_uppercase(char *name)
-{
-    unsigned int i;
-
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
-    printf("\n");
-}
