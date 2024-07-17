beead6e16d64f84a613a88c00d300f37c7639a3c
Fareedizm <guliyevfarid013.com>
2024-07-17 14:05:57 +0400

    function_pointers

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -6,15 +6,16 @@
  * @size: Number of elements in the array
  * @cmp: Pointer to the function used to compare values
  */
-int int_index(int *array, int size, int (*cmp)(int))
-{
+	int int_index(int *array, int size, int (*cmp)(int))
+	{
        		int i;
 
 	if (array != NULL && cmp != NULL && size > 0)
 	{
 			for (i = 0; i < size; i++)
 	{
-	if (cmp(array[i]) !=0); 
+	if (cmp(array[i]) !=0)
+		       	
 		return i;
 	}
 	}
