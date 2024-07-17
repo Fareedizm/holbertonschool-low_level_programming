d82f22221b7ffcb82f80882b3175bfc64d3c6ce3
Fareedizm <guliyevfarid013.com>
2024-07-17 13:36:04 +0400

    function_pointers

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,15 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
+/**
+ * print_name - func
+ * @name: string
+ * @f: func
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-        f(name);
+	if (name != NULL && f != NULL)
+		if(name);
 }
 
