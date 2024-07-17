71b5e0821dac2dcdaec2ba0bc8582566a95dcf2c
Fareedizm <guliyevfarid013.com>
2024-07-17 13:55:02 +0400

    function_pointers

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,20 +5,17 @@
  * @array: Pointer to the array
  * @size: Size of the array
  * @action: Pointer to the function to execute on each array element
- *
- * Description: This function iterates through the array elements and applies
- * the function `action` to each element.
  */
 	void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	    	size_t i;
 
     	if (array != NULL && action != NULL)
-    {
-       		 for (i = 0; i < size; i++)
-        {
-             	action(array[i]);
-        }
-    }
-}
+{
+		for (i = 0; i < size; i++)
+	{
+			action(array[i]);
+	}
+	}
+	}
 
