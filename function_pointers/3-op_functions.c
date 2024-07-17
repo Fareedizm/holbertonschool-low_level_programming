344dba81e65042928d28b16fc2fc3f5d776e2d7c
Fareedizm <guliyevfarid013.com>
2024-07-17 14:10:51 +0400

    function_pointers

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+
+/* Function to add two integers */
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/* Function to subtract two integers */
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/* Function to multiply two integers */
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/* Function to divide two integers */
+int op_div(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    return (a / b);
+}
+
+/* Function to calculate the modulus of two integers */
+int op_mod(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    return (a % b);
+}
+
