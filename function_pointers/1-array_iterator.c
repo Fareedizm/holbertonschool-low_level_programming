4123913141ca201026e0bc0ac5242892c7583cb3
Fareedizm <guliyevfarid013.com>
2024-07-17 13:51:54 +0400

    function_pointers

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,16 +1,24 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - execute a function pointer on each element of an array
- * @array: array of integers
- * @size: size of array
- * @action: function pointer
+ * array_iterator - Executes a function given as a parameter on each element of an array
+ * @array: Pointer to the array
+ * @size: Size of the array
+ * @action: Pointer to the function to execute on each array element
+ *
+ * Description: This function iterates through the array elements and applies
+ * the function `action` to each element.
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	    size_t i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+    	if (array != NULL && action != NULL)
+    {
+       		 for (i = 0; i < size; i++)
+        {
+            			action(array[i]);
+        }
+    }
 }
+
--- function_pointers/main_0.c
+++ function_pointers/main_0.c
@@ -7,3 +7,11 @@ int main(void)
     print_name("Bob Dylan", print_name_uppercase);
     return 0;
 }
+int main(void)
+{
+    int array[5] = {0, 98, 402, 1024, 4096};
+
+    array_iterator(array, 5, &print_elem);
+    array_iterator(array, 5, &print_elem_hex);
+    return 0;
+}
